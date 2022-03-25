aa1f69b373a57ea2675f054cb009f82b473669f8
Loren <lorenezratty@gmail.com>
2022-03-24 23:21:43 -0500

    upgrading prep__ethereum_contracts with surrogate keys and formatting

--- dbt/models/prep/prep__ethereum_contracts.sql
+++ dbt/models/prep/prep__ethereum_contracts.sql
@@ -9,15 +9,22 @@ contracts as (
 final as (
 
     select
-        contracts.block_hash,
-        contracts.block_number,
-        contracts.ethereum_address,
-        contracts.bytecode,
-        contracts.function_sighashes,
-        contracts.is_erc20,
-        contracts.is_erc721,
-        contracts.block_created_at,
-        {{ dbt_utils.surrogate_key(['block_hash', 'ethereum_address']) }} as surrogate_transaction_key
+        -- PRIMARY DIMENSIONS
+        contracts.block_hash                                                as block_hash,
+        contracts.block_number                                              as block_number,
+
+        -- ADDITIONAL ATTRIBUTES
+        contracts.bytecode                                                  as bytecode,
+        contracts.function_sighashes                                        as function_sighashes,
+        contracts.is_erc20                                                  as is_erc20,
+        contracts.is_erc721                                                 as is_erc721,
+
+        -- FOREIGN KEYS
+        contracts.ethereum_address                                          as ethereum_address,
+        {{ dbt_utils.surrogate_key(['block_hash', 'ethereum_address']) }}   as surrogate_transaction_key,
+
+        -- METADATA
+        contracts.block_created_at                                          as block_created_at
 
     from contracts
 
